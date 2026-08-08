0439f6bae14e0c35fbe443a2861c42503d8493e7
hothachthien <thachthienho46@gmail.com>
2026-08-08 08:58:05 +0700

    Viet ham nhapMang xuatMang nguyen

--- quanlymang.c
+++ quanlymang.c
@@ -1,5 +1,28 @@
-#include<stdio.h>
-   int main(){
-      printf("a");
-    return 0;
+#include <stdio.h>
+void nhapMang(int a[], int n);
+void xuatMang(int a[], int n);
+int main()
+{
+   int n = 5;
+   int mangA[n];
+   nhapMang(mangA, n);
+   xuatMang(mangA, n);
+   return 0;
+}
+void nhapMang(int a[], int n)
+{
+   printf("Nhap mang: \n");
+   for (int i = 0; i < n; i++)
+   {
+      printf("Nhap A[%d]= ", i);
+      scanf("%d", &a[i]);
    }
+}
+void xuatMang(int a[], int n)
+{
+   printf("Xuat mang: \n");
+   for (int i = 0; i < n; i++)
+   {
+      printf("A[%d]= %d\n", i, a[i]);
+   }
+}
